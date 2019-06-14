d4fbb710d757e4ad4e9fc24cfdf3c1cfbcd1e514
Krzysztof Kacprzak <k.kacprzak@oberthur.com>
2019-06-14 21:20:47 +0200

    Async set state for event with menu

--- custom_components/lunch/__init__.py
+++ custom_components/lunch/__init__.py
@@ -14,16 +14,24 @@ def get_as_base64(url):
     return base64.b64encode(requests.get(url).content)
 
 async def async_setup(hass, config):
-   
+
+    @callback
+    def menu_received(event):
+        hass.states.async_set(event.attributes.id, event.attributes)
+
+    await hass.bus.listen(DOMAIN, menu_received)
+
     @callback
     def get_menu(call):
         fbUrl = call.data.get('url')
         name = call.data.get('name')
+        id = normalize_name(name)
 
         restaurant = pq(url=fbUrl)
         lastPost = restaurant("div").filter(".userContentWrapper")[0]
 
         menu = {}
+        menu['id'] = id
         menu['name'] = restaurant("title").text()
         menu['timestamp'] = pq(lastPost)("span").filter(".timestampContent").text()
         menu['text'] = pq(lastPost)("div[data-testid='post_message'] ").text()
@@ -34,7 +42,8 @@ async def async_setup(hass, config):
             dataSrc = pq(img).attr("data-src")
             if dataSrc is not None:
                 menu['img'].append("data:image/jpeg;base64," + str(get_as_base64(dataSrc), 'utf-8'))
-        hass.bus.fire('lunch.' + normalize_name(name), menu)
+        hass.bus.fire(id, menu)
+        hass.bus.fire(DOMAIN, menu)
     
 
     # Register our service with Home Assistant.

5bb11d4440cce6cf0fe039f87cbdfd212a7c2360
Krzysztof Kacprzak <k.kacprzak@oberthur.com>
2019-06-14 20:34:06 +0200

    Normalize spaces in lunch

--- custom_components/lunch/__init__.py
+++ custom_components/lunch/__init__.py
@@ -8,7 +8,7 @@ import requests
 DOMAIN = "lunch"
 
 def normalize_name(name):
-	return re.sub('([a-z0-9])([A-Z])', r'\1_\2', re.sub('(.)([A-Z][a-z]+)', r'\1_\2', strip(name))).lower()
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', re.sub('(.)([A-Z][a-z]+)', r'\1_\2', strip(name))).lower()
 
 def get_as_base64(url):
     return base64.b64encode(requests.get(url).content)
@@ -17,24 +17,24 @@ async def async_setup(hass, config):
    
     @callback
     def get_menu(call):
-    	fbUrl = call.data.get('url')
-    	name = call.data.get('name')
+        fbUrl = call.data.get('url')
+        name = call.data.get('name')
 
-    	restaurant = pq(url=fbUrl)
-    	lastPost = restaurant("div").filter(".userContentWrapper")[0]
+        restaurant = pq(url=fbUrl)
+        lastPost = restaurant("div").filter(".userContentWrapper")[0]
 
-    	menu = {}
-    	menu['name'] = restaurant("title").text()
-    	menu['timestamp'] = pq(lastPost)("span").filter(".timestampContent").text()
-    	menu['text'] = pq(lastPost)("div[data-testid='post_message'] ").text()
-    	menu['img'] = []
+        menu = {}
+        menu['name'] = restaurant("title").text()
+        menu['timestamp'] = pq(lastPost)("span").filter(".timestampContent").text()
+        menu['text'] = pq(lastPost)("div[data-testid='post_message'] ").text()
+        menu['img'] = []
 
-    	images = pq(lastPost)("img")
-    	for img in images:
-    		dataSrc = pq(img).attr("data-src")
-    		if dataSrc is not None:
-    			menu['img'].append("data:image/jpeg;base64," + str(get_as_base64(dataSrc), 'utf-8'))
-		hass.states.set('lunch.' + normalize_name(name), any(menu), menu, True)
+        images = pq(lastPost)("img")
+        for img in images:
+            dataSrc = pq(img).attr("data-src")
+            if dataSrc is not None:
+                menu['img'].append("data:image/jpeg;base64," + str(get_as_base64(dataSrc), 'utf-8'))
+        hass.states.set('lunch.' + normalize_name(name), any(menu), menu, True)
     
 
     # Register our service with Home Assistant.

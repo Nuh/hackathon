20d681cfb3cee1579faeaba4b59f67537b0383a4
Jakub SZEWCZYK <jakub.szewczyk@idemia.com>
2019-06-14 18:22:25 +0200

    lunch.py another fix

--- custom_components/lunch/__init__.py
+++ custom_components/lunch/__init__.py
@@ -35,39 +35,3 @@ async def async_setup(hass, config):
 
     # Return boolean to indicate that initialization was successfully.
     return True
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-fbUrl = data.get('url')
-name = data.get('name')
-
-restaurant = pq(url=fbUrl)
-lastPost = restaurant("div").filter(".userContentWrapper")[0]
-
-menu = {}
-menu['name'] = restaurant("title").text()
-menu['timestamp'] = pq(lastPost)("span").filter(".timestampContent").text()
-menu['text'] = pq(lastPost)("div[data-testid='post_message'] ").text()
-menu['img'] = []
-
-images = pq(lastPost)("img")
-for img in images:
-	dataSrc = pq(img).attr("data-src")
-	if dataSrc is not None:
-		menu['img'].append("data:image/jpeg;base64," + str(get_as_base64(dataSrc), 'utf-8'))
-		
-
-hass.bus.fire(name, menu)

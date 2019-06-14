379e0fa11c62e44ecac26f8c44ed4c71ebd2be56
Krzysztof Kacprzak <k.kacprzak@oberthur.com>
2019-06-14 22:45:36 +0200

    Reorganization lunch

--- custom_components/lunch/__init__.py
+++ custom_components/lunch/__init__.py
@@ -1,22 +1,24 @@
-from homeassistant.core import callback
-from logging import getLogger
-from pyquery import PyQuery as pq
 import base64
 import re
 import requests
+from homeassistant.core import callback
+from logging import getLogger
+from pyquery import PyQuery as pq
 
 LOGGER = getLogger(__name__)
 
 DOMAIN = "lunch"
 
+
 def normalize_name(name):
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', re.sub('(.)([A-Z][a-z]+)', r'\1_\2', str(name))).lower()
+
 
 def get_as_base64(url):
     return base64.b64encode(requests.get(url).content)
 
-async def async_setup(hass, config):
 
+async def async_setup(hass, config):
     @callback
     def menu_received(event):
         if event and event.data and event.data.id:
@@ -41,12 +43,21 @@ async def async_setup(hass, config):
             for img in pq(last_post)("img"):
                 src = pq(img).attr("data-src")
                 if src is not None:
-                    images.append({'url': src, 'data': "data:image/jpeg;base64," + str(get_as_base64(src), 'utf-8')})
+                    images.append({
+                        'url': src,
+                        'data': "data:image/jpeg;base64," + str(get_as_base64(src), 'utf-8')
+                    })
 
-            menu = {'name': name, 'friendly-name': friendly_name, 'title': title, 'timestamp': pq(last_post)("span").filter(".timestampContent").text(), 'text': pq(last_post)("div[data-testid='post_message'] ").text()}
+            menu = {
+                'name': name,
+                'friendly-name': friendly_name,
+                'title': title,
+                'timestamp': pq(last_post)("span").filter(".timestampContent").text(),
+                'text': pq(last_post)("div[data-testid='post_message'] ").text()
+            }
+
             hass.bus.fire(name, menu)
             hass.bus.fire(DOMAIN, menu)
-    
 
     # Register our service with Home Assistant.
     hass.services.async_register(DOMAIN, 'get_menu', get_menu)

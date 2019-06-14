d3fc81617c1ab6599c6b96299a04c88a217ce8bf
Jakub SZEWCZYK <jakub.szewczyk@idemia.com>
2019-06-14 17:48:50 +0200

    lunch as custom_component

---
+++ custom_components/lunch/__init__.py
@@ -0,0 +1,74 @@
+from homeassistant.core import callback
+from pyquery import PyQuery as pq
+
+# The domain of your component. Should be equal to the name of your component.
+DOMAIN = "lunch"
+
+async def async_setup(hass, config):
+   
+    # Service to publish a message on MQTT.
+    @callback
+    def get_menu(call):
+    	fbUrl = data.get('url')
+    	name = data.get('name')
+
+    	restaurant = pq(url=fbUrl)
+    	lastPost = restaurant("div").filter(".userContentWrapper")[0]
+
+    	menu = {}
+    	menu['name'] = restaurant("title").text()
+    	menu['timestamp'] = pq(lastPost)("span").filter(".timestampContent").text()
+    	menu['text'] = pq(lastPost)("div[data-testid='post_message'] ").text()
+    	menu['img'] = []
+
+    	images = pq(lastPost)("img")
+    	for img in images:
+    		dataSrc = pq(img).attr("data-src")
+    		if dataSrc is not None:
+    			menu['img'].append("data:image/jpeg;base64," + str(get_as_base64(dataSrc), 'utf-8'))
+    			
+
+    	hass.bus.fire(name, menu)    
+    
+
+    # Register our service with Home Assistant.
+    hass.services.async_register(DOMAIN, 'get_menu', get_menu)
+
+    # Return boolean to indicate that initialization was successfully.
+    return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fbUrl = data.get('url')
+name = data.get('name')
+
+restaurant = pq(url=fbUrl)
+lastPost = restaurant("div").filter(".userContentWrapper")[0]
+
+menu = {}
+menu['name'] = restaurant("title").text()
+menu['timestamp'] = pq(lastPost)("span").filter(".timestampContent").text()
+menu['text'] = pq(lastPost)("div[data-testid='post_message'] ").text()
+menu['img'] = []
+
+images = pq(lastPost)("img")
+for img in images:
+	dataSrc = pq(img).attr("data-src")
+	if dataSrc is not None:
+		menu['img'].append("data:image/jpeg;base64," + str(get_as_base64(dataSrc), 'utf-8'))
+		
+
+hass.bus.fire(name, menu)
